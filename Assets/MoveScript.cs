703701c8584e5f39b627609b38075b6ae2eede82
Andreas Eickhoff <ateickhoff@oakland.edu>
2019-04-02 12:58:31 -0500

    Added Menu and hub
    assets and scripts

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    //Speed is accessable to all
+    public float speed;
+    private Rigidbody move;
+    public float error;
+
+    void Start()
+    {
+    move = GetComponent<Rigidbody> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown("d")) 
+        {
+            Vector3 movement = new Vector3(1500, 0);
+        move.AddForce(movement*speed);
+
+            //Rotation fixing
+            if (System.Math.Abs(move.rotation.eulerAngles.y - 180) > error )
+            {
+                Vector3 rotate = new Vector3(0, (180 - move.rotation.eulerAngles.y), 0);
+                move.AddTorque(rotate * 10);
+            }
+
+        }
+
+
+        if (Input.GetKeyDown("a"))
+        {
+            Vector3 movement = new Vector3(-1500, 0);
+            move.AddForce(movement * speed);
+
+            //Rotation fixing
+            if (System.Math.Abs(move.rotation.eulerAngles.y) > error)
+            {
+
+                Vector3 rotate = new Vector3(0, move.rotation.eulerAngles.y-180, 0);
+                move.AddTorque(rotate * 600);
+            }
+
+        }
+
+    }
+}
